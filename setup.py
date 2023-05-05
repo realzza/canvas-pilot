d8bbd3f51f39d3fc3993763ce648979b797e14b8
ziang <ziangz@andrew.cmu.edu>
2023-05-04 21:05:15 -0400

    release 0.1.2 canvas-pilot

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="canvas-pilot",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         "requests",
